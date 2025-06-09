6489ef12c861262bc8831b1e6393017591a912f8
Tyler Westland <tylerofthewest@gmail.com>
2025-06-08 21:15:59 -0500

    Changes up Rectangle to be before Circle

--- code/main.py
+++ code/main.py
@@ -3,19 +3,19 @@ from shapes.rectangle import Rectangle
 from shapes.circle import Circle
 
 
-circ = Circle(3)
-print(f"circ area: {circ.area():.2f}")
-
-circ2 = Circle(5)
-print(f"circ2 area: {circ2.area():.2f}")
-
-print(f"circ.area() > circ2.area(): {circ.area() > circ2.area()}")
-
 rect = Rectangle(4, 5)
 print(f"rect area: {rect.area()}")
+
+rect2 = Rectangle(3,8)
+print(f"rect2 area: {rect2.area():.2f}")
+
+print(f"rect.area() > rect2.area(): {rect.area() > rect2.area()}")
+
+circ = Circle(3)
+print(f"circ area: {circ.area():.2f}")
 
 print(f"circ.area() > rect.area(): {circ.area() > rect.area()}")
 
 
-print(f"circ > circ2: {circ > circ2}")
+print(f"rect > rect2: {rect > rect2}")
 print(f"circ > rect: {circ > rect}")
